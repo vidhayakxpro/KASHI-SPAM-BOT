02a4f3964283d7c7715f6a57736425af599f7209
FRANCIUM444 <151495276+FRANCIUM444@users.noreply.github.com>
2024-02-05 15:55:15 +0530

    Update logs.py

--- AltBots/modules/logs.py
+++ AltBots/modules/logs.py
@@ -42,7 +42,7 @@ async def logs(legend):
         fetch = await legend.reply(f"__Fetching Logs...__")
     
         with open("AltLogs.txt", "w") as logfile:
-            logfile.write("⚡ ™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍 ⚡ [ Bot Logs ]\n\n" + logs)
+            logfile.write("⚡ ᵛⁱᵖ° 🫧˹🇫ᏒΔṆℭỉ𝖴𝐌 𓆩ꭙᗪ𓆪 ˼ ⚡ [ Bot Logs ]\n\n" + logs)
 
         end = datetime.now()
         ms = (end-start).seconds
